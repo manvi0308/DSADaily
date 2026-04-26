2ca1aea93d83117081b1c330523aa26fe99119ba
manvi0308 <chaddhamanvi3@gmail.com>
2026-04-26 18:48:43 +0530

    Added medium questions

---
+++ basicquestions/ModifyMatrix.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class ModifyMatrix {
+    public static int [][] modifiedMatrix(int[][] matrix){
+        int[][] res = new int[matrix.length][matrix[0].length];
+        int[] maxValueForCol = new int[matrix[0].length];
+        Arrays.fill(maxValueForCol, -1);
+        // iterate over each column
+        for (int j = 0; j < matrix[0].length; j++){
+            for (int i = 0; i < matrix.length; i++){
+                maxValueForCol[j] =  Math.max(maxValueForCol[j], matrix[i][j]);
+            }
+        }
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++){
+                if (matrix[i][j] != -1){
+                    res[i][j] = matrix[i][j];
+                } else{
+                    res[i][j] = maxValueForCol[j];
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        // int[][] matrix = {{1, 2, -1}, {4, -1, 6}, {7, 8, 9}};
+        int[][] matrix = {{3, -1}, {5, 2}};
+        int[][] ans = modifiedMatrix(matrix);
+        for (int i = 0; i < ans.length; i++){
+            System.out.println(Arrays.toString(ans[i]));
+        }
+    }
+}
---
+++ basicquestions/SearchInRotatedSortedArray.java
@@ -0,0 +1,34 @@
+public class SearchInRotatedSortedArray {
+    public static boolean search(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high){
+            int mid = low + ( high - low )/2;
+            if (nums[mid] == target)
+                return true;
+            if ((nums[low] == nums[mid]) && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if (nums[low] <= nums[mid]){
+                if (nums[low] <= target && nums[mid] >= target)
+                    high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(nums[mid] <= target && nums[high] >= target)
+                    low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,0,0,1,2};
+        int target = 0;
+
+        System.out.println(search(nums, target));
+    }
+}
