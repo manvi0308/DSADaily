db2a3de5923eddd5271769fc926f77cc2240d6ec
manvi0308 <chaddhamanvi3@gmail.com>
2026-05-13 00:03:07 +0530

    added more code

---
+++ dsa/strings/LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++){
+            if (first.charAt(i) != last.charAt(i)){
+                return sb.toString();
+            }
+            sb.append(first.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flowe", "flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
