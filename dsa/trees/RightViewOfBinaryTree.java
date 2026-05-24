25f0f726d1aae48629a0fdbc0f170f230ae7df1a
manvi0308 <chaddhamanvi3@gmail.com>
2026-05-24 22:51:50 +0530

    added level order by DFS

--- dsa/trees/RightViewOfBinaryTree.java
+++ dsa/trees/RightViewOfBinaryTree.java
@@ -1,45 +1,27 @@
 package trees;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class RightViewOfBinaryTree {
-    class TreeNode{
-        int data;
-        TreeNode left;
-        TreeNode right;
+    /*
+            1
+         2      3
+            5      4
 
-        public TreeNode(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
+        O/P -> 1, 3, 4
+     */
+    public List<Integer> rightView(TreeNode root){
+        List<Integer> res = new ArrayList<>();
+        rightDFS(root, 0, res);
+        return res;
     }
-/*
-         Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        List<List<Integer>> ans = new ArrayList<>();
-        if (root == null)
-            return ans;
-        queue.add(root);
-        while (!queue.isEmpty()){
-            int count = queue.size();
-            List<Integer> nodes = new ArrayList<>();
-            for (int i = 0; i < count; i++){
-                if (queue.peek().left != null)
-                    queue.add(queue.peek().left);
-                if (queue.peek().right != null)
-                    queue.add(queue.peek().right);
-                nodes.add(queue.remove().val);
-            }
-            ans.add(nodes);
-        }
-        return ans;*/
-public List<Integer> rightView(TreeNode root){
-        List<Integer> ans = new ArrayList<>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        
-        return ans;
+    public void rightDFS(TreeNode node, int level, List<Integer> list){
+        if (node == null)
+            return;
+        if (list.size() == level)
+            list.add(node.val);
+        rightDFS(node.right, level + 1, list);
+        rightDFS(node.left, level + 1, list);
     }
 }
